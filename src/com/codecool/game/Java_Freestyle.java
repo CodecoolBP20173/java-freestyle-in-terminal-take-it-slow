e630ee67b7791feec2a0cd71ff4392e61ee39c22
BalazsParducz <parducz.balazs@gmail.com>
2018-03-15 21:27:18 +0100

    able to draw given number of cards and colour the first one

--- src/com/codecool/game/Java_Freestyle.java
+++ src/com/codecool/game/Java_Freestyle.java
@@ -1,13 +1,19 @@
 package com.codecool.game;
 
+import com.codecool.termlib.Color;
+import com.codecool.termlib.Direction;
 import com.codecool.termlib.Terminal;
 
 public class Java_Freestyle {
 
     public static void main(String[] args) {
 
-        System.out.println(Terminal.CLEAR);
-
+        Terminal terminal = new Terminal();
+        terminal.clearScreen();
+        terminal.moveTo(10, 10);        
+        terminal.drawCard(4);
+        terminal.flipCard();
+        
     };
 
 }
